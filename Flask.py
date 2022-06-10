6a3bb85dd93b6897e840dac965884f3568bf8172
AlexSG <alexanderstopper@hotmail.com>
2022-06-10 15:19:26 -0500

    cambios

--- Flask.py
+++ Flask.py
@@ -1,16 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 # Inicializar la aplicacion
 app = Flask(__name__, template_folder='templates')
 
-# Ruta principal para el login.
-@app.route('/')
-def login():
-    return render_template('Login.html')
-#Ruta para llamara a registro.
-@app.route('/Registrarse')
-def registro():
-    return render_template('Registro.html')
+#arreglos
+login_User=[]
+
+
 #Ruta para llamara a recuperar contraseña.
 @app.route('/Recuperar_Contraseña')
 def recuperarCo():
@@ -40,6 +36,46 @@ def contactos():
 def about():
     return render_template('About.html')
 
+# Ruta principal para el login.
+@app.route('/', methods=['GET','POST'])
+def Login():
+    #Obtiene los datos del pagina Login por medio del metodo POST
+    if(request.method == "POST"):
+        Email = request.form['email']  #O  btencion de correo
+        Password = request.form['Password']  #Obtencion de contraseña
+
+
+        #try para evaluar si un correo no se encuentra en la lista
+        try:
+            if (login_User.index(Email) >= 0):    #obtener el correo
+                Indice = login_User.index(Email)
+                if(login_User[Indice] == Email and login_User[Indice+1] == Password):   # Compara con el registro
+                    return redirect(url_for('principal'))    # Nos envia a la pagina principal o inicio
+                else:
+                    return redirect(url_for('Login'))
+        except:
+            return redirect(url_for('Login'))
+
+    return render_template('Login.html')
+
+#Ruta para llamara a registro.
+@app.route('/Registro' , methods=['GET','POST'])
+def Registro():
+    if(request.method == "POST"): #Obtiene los datos por medio del metodo POST
+        nombre = request.form['pnombre']
+        apellidos = request.form['Apellido']
+        email = request.form['Email']         
+        password = request.form['password']
+        if(nombre == "" or apellidos == "" or email  == "" or password == ""):    #Condicion para direccionar si no hay datos
+            return redirect(url_for('Registro'))
+        else:
+            #Ingresa datos al array
+            login_User.append(nombre)
+            login_User.append(apellidos)
+            login_User.append(email)
+            login_User.append(password)
+            return redirect(url_for('Login'))
+    return render_template('Registro.html')
 
 # Para ejecutar la aplicacion
 if __name__ == '__main__':
