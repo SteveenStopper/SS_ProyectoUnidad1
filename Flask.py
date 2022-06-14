d3876734993d4f8434b595083ef3016a9583922a
AlexSG <alexanderstopper@hotmail.com>
2022-06-13 22:55:41 -0500

    Cambios realizados en el proyecto

--- Flask.py
+++ Flask.py
@@ -1,3 +1,4 @@
+from operator import length_hint
 from flask import Flask, render_template, request, redirect, url_for
 
 # Inicializar la aplicacion
@@ -5,32 +6,62 @@ app = Flask(__name__, template_folder='templates')
 
 #arreglos
 login_User=[]
+Ag_Citas=[]
 
 
 #Ruta para llamara a recuperar contraseña.
 @app.route('/Recuperar_Contraseña')
 def recuperarCo():
     return render_template('RecuperarC.html')
+
 #Ruta para llamara a la pagina principal.
 @app.route('/Inicio')
 def principal():
     return render_template('Principal.html')
+
 #Ruta para llamara a la pagina para agendar citas.
-@app.route('/Agendar_Cita')
-def agCita():
-    return render_template('AgCitas.html')
+@app.route('/Agendar_Cita', methods=['GET','POST'])
+def Agendar_Cita():
+    if(request.method == "POST"):
+        #Instanciamiento de datos de nuestra pagina Citas
+        Nombre = request.form['pnombre']          
+        Apellido = request.form['papellido'] 
+        Dia = request.form['date']
+        Email = request.form['email']      
+        Trata = request.form['treatment']
+
+        #(Condicional) Si el formulario no tiene ingresado datos no registrara
+        if(Nombre == "" or Apellido  == "" or Dia == "" or Email == ""  or Trata == ""):
+            return redirect(url_for('Agendar_Cita'))
+        else:
+            id_pac = len(Ag_Citas) + 1
+            Ag_Citas.append({'id_pac' : id_pac,'pnombre': Nombre, 'papellido': Apellido, 'date': Dia, 'email': Email,
+            'treatment': Trata})
+            print(Ag_Citas)
+    return render_template('AgCitas.html', Og_Citas = Ag_Citas)
+
+#Para borrar
+@app.route('/borrar', methods=["GET","POST"])
+def borrar():   
+    if(request.method == "POST"):        
+        Ag_Citas.clear()        
+    return redirect(url_for('Agendar_Cita'))
+
 #Ruta para llamara a Ortodoncia.
 @app.route('/Ortodoncia')
 def ortodoncia():
     return render_template('Ortodoncia.html')
+
 #Ruta para llamara a odontopediatria.
 @app.route('/Odontopediatria')
 def odonto():
     return render_template('Odontopediatria.html')
+
 #Ruta para llamara a contacto.
 @app.route('/Contacto')
 def contactos():
     return render_template('Contactos.html')
+
 #Ruta para llamara a sobre nosotros o About.
 @app.route('/Sobre_Nosotros')
 def about():
@@ -77,6 +108,6 @@ def Registro():
             return redirect(url_for('Login'))
     return render_template('Registro.html')
 
-# Para ejecutar la aplicacion
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) # Para ejecutar la aplicacion
